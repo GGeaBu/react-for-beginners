5368e687799b40dd1fe42ba185a5aeae6e0b6438
GgeaBu <h2sorginal@gmail.com>
2022-04-14 00:20:39 +0900

    Modifying Movie.css and Home.css coding

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import style from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -17,11 +18,11 @@ function Home() {
       getMovies();
     }, []);
     return (
-      <div>
+      <div className={style.container}>
         {loading ? (
           <h1>Loading...</h1>
         ) : (
-          <div>
+          <div className={style.movies}>
             {movies.map(movie => (
               <Movie 
                 key={movie.id}
