2d23db69bec175f61f4391f7a55c69131ea16a8f
GgeaBu <h2sorginal@gmail.com>
2022-04-13 16:22:36 +0900

    react beginner projects

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootNode = document.getElementById('root');
+
+ReactDOM.createRoot(rootNode).render(
+  <App />,
+);
